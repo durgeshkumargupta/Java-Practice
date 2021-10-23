352d6901b0ffb3cf3c2e35011da8da177c762685
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-10-23 11:59:21 +0530

    Add files via upload

---
+++ Database/DatabaseConnection/Program-2/MyException.java
@@ -0,0 +1,12 @@
+package com.durgesh.program6;
+
+public class MyException extends Exception 
+{
+	private static final long serialVersionUID = 9135912387349545065L;
+
+	MyException(String message)
+	{
+		System.out.println("You got exception !!"+ message);
+	}
+
+}
